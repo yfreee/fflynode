34e8d7b30551d127c1f128d47cf437be90ea1d5c
Jeff P <jeffrey.perez@avantica.net>
2016-05-17 16:48:11 -0600

    lets start

---
+++ test.js
@@ -0,0 +1,9 @@
+var querystring = require('querystring');
+var https = require('https');
+
+function getContents(host){
+    //var url = JSON.stringify(querystring);
+    console.info('getting content from'+host);
+    
+    
+}
