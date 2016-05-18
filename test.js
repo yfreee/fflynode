7fc7f77a20bbc59ebca66865fca919a6ea7db4cb
Jeff P <jeffrey.perez@avantica.net>
2016-05-18 10:00:43 -0600

    few mod

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 var querystring = require('querystring');
 var https = require('https');
-
+var express = require('express');
 function getContents(host){
     //var url = JSON.stringify(querystring);
     console.info('getting content from'+host);
-    
+
     
 }
